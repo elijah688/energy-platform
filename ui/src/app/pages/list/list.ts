fa304c826d47ca556e7c14658cefe224d5fe180d
Elijah Iliev <elijah@MacBookPro.lan>
2025-10-30 17:02:29 +0000

    transactions ok

--- ui/src/app/pages/list/list.ts
+++ ui/src/app/pages/list/list.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
 import { UserService } from '../../services/users';
 import { TransactionService } from '../../services/transaction';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatButtonModule } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [MatCardModule, MatExpansionModule, MatButtonModule, RouterLink],
+  imports: [MatCard, MatExpansionPanel, MatAccordion, MatExpansionPanelHeader, MatExpansionPanelTitle, RouterLink, MatButtonModule],
   templateUrl: './list.html',
   styleUrls: ['./list.sass']
 })
-export class List implements OnInit {
+export class List implements OnInit, AfterViewInit {
   userServ = inject(UserService);
   transService = inject(TransactionService);
+
+  @ViewChildren(MatAccordion) accordions!: QueryList<MatAccordion>;
 
   async ngOnInit() {
     await this.userServ.fetchUsers();
   }
 
+  ngAfterViewInit() {
+    // All accordions are available here
+  }
+
   async nextPage() {
     await this.userServ.next();
+    this.closeAllAccordions();
   }
 
   async prevPage() {
     await this.userServ.prev();
+    this.closeAllAccordions();
+  }
+
+  closeAllAccordions() {
+    this.accordions.forEach(acc => acc.closeAll());
   }
 
   async fetchUserTransactions(userId: string) {
