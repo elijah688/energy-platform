0de5d9345325e9ca3e322c0b7b9156a7108302d0
Elijah Iliev <elijah@MacBookPro.lan>
2025-11-02 20:35:15 +0000

    ui - layout

---
+++ ui/src/app/layout/layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './layout.html',
+  styleUrl: './layout.sass',
+})
+
+export class Layout {
+
+}
