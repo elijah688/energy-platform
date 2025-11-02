88ca9bca2f27c7748b3fdedbf732966927215198
Elijah Iliev <elijah@MacBookPro.lan>
2025-11-02 20:29:49 +0000

    cmd - dg - main

---
+++ api/cmd/GeneratorDaemon/Program.cs
@@ -0,0 +1,22 @@
+﻿using GeneratorDaemon.src.GeneratorProcess;
+
+namespace GeneratorDaemon
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            while (true)
+            {
+                var gp = new GeneratorProcess();
+
+                await gp.RunAsync();
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+            }
+
+        }
+
+    }
+}
