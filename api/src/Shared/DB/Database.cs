7099b3d34f5096ab4653a734e5ef13083694bbc7
Elijah Iliev <elijah@MacBookPro.lan>
2025-11-02 20:25:14 +0000

    db: connection abs

---
+++ api/src/Shared/DB/Database.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+
+namespace Shared.DB
+{
+    public abstract class BaseDB
+    {
+        private static readonly string _connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? "";
+
+        protected static NpgsqlConnection GetConnection()
+        {
+            var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
